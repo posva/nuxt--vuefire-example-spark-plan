f4c13e7fbfbb3500e5313a172c393b72e498a732
Eduardo San Martin Morote <posva13@gmail.com>
2023-07-31 16:34:24 +0200

    firebase init + deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: ['nuxt-vuefire'],
+
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyBKBqCHUpxMNjRJ8uhgOTK0wMGr9LkkFOA",
+      authDomain: "nuxt-vuefire-example-spark.firebaseapp.com",
+      databaseURL: "https://nuxt-vuefire-example-spark-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "nuxt-vuefire-example-spark",
+      storageBucket: "nuxt-vuefire-example-spark.appspot.com",
+      messagingSenderId: "639475067598",
+      appId: "1:639475067598:web:13fc8572370163aa913e9f",
+      measurementId: "G-V3RKC1RZG1"
+    }
+  }
 })
